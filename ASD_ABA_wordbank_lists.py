d5ca9f9b13f53d71a3e097c168fd96227302896b
andrea-mar <andreanistormar@gmail.com>
2023-08-09 10:19:37 +0100

    aba_wordbank final and asd_wordbank databases created - TODO write to file and remove irrelevant terms

---
+++ ASD_ABA_wordbank_lists.py
@@ -0,0 +1,30 @@
+wb_list_ASD_ABA_global = [
+    'rq_world_df_rising', 
+    'rq_world_df_top',
+    ]
+
+
+wb_list_ASD_ABA_uk = [
+    'rq_uk_df_rising',
+    'rq_uk_df_top' 
+    ]
+
+
+wb_list_ASD_global = [
+    'rq_asd_disorder_global_df_rising',
+    'rq_asd_disorder_global_df_top',
+    'rq_asd_search_term_global_df_rising',
+    'rq_asd_search_term_global_df_top',
+    'rq_autism_search_term_global_df_rising',
+    'rq_autism_search_term_global_df_top'
+    ]
+
+
+wb_list_ASD_uk = [
+    'rq_asd_disorder_uk_df_rising', 
+    'rq_asd_disorder_uk_df_top',
+    'rq_asd_search_term_uk_df_rising',
+    'rq_asd_search_term_uk_df_top',
+    'rq_autism_search_term_uk_df_rising',
+    'rq_autism_search_term_uk_df_top'
+    ]
