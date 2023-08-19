8485794064f89bc422e3198ecb982d419f0b3270
andrea-mar <andreanistormar@gmail.com>
2023-08-19 20:57:29 +0100

    private subreddits mods contact script - open tabs and fill in text, Captcha needs to be done manually

--- ASD_ABA_wordbank_lists.py
+++ ASD_ABA_wordbank_lists.py
@@ -30,3 +30,63 @@ wb_list_ASD_uk = [
     'rq_autism_search_term_uk_df_rising',
     'rq_autism_search_term_uk_df_top'
     ]
+
+
+asd_wordbank_set = {
+    'asd',
+    'asd disorder',
+    'asd spectrum',
+    'asperger',
+    'aspergers',
+    'aspergers autism',
+    'autism',
+    'autism disorder',
+    'autism spectrum',
+    'autism spectrum disorder',
+    'autistic',
+    'autistic spectrum disorder',
+    'autistic spectrum disorders',
+    'high functioning autism',
+    'high-functioning autism spectrum disorder',
+    'neurodivergent'
+}
+
+aba_wordbank_set = {
+    'aba',
+    'aba behavior',
+    'aba behavior therapy',
+    'aba behavioral therapy',
+    'aba conversion therapy',
+    'aba home therapy',
+    'aba school',
+    'aba services',
+    'aba therapist',
+    'aba therapy',
+    'aba therapy center',
+    'aba therapy school',
+    'aba therapy services',
+    'aba training',
+    'aba tutor',
+    'acceptance and commitment therapy',
+    'applied behavior analysis',
+    'applied behaviour analysis',
+    'bcaba',
+    'bcba',
+    'behavior',
+    'behavior analysis',
+    'behavior analyst',
+    'behavior technician',
+    'behavior therapy',
+    'behavioral analysis',
+    'behavioral therapy',
+    'behaviorism',
+    'behaviour',
+    'behaviour analysis',
+    'behaviour technician',
+    'behavioural analysis',
+    'discrete trial training',
+    'dtt',
+    'rbt',
+    'registered behavior technician',
+    'registered behaviour technician'
+}
